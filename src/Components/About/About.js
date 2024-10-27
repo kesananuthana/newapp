5ac713f70342ca69639f4a4c49019f481c4a6173
kesananuthana <kesananutana@gmail.com>
2024-10-27 10:40:37 +0530

    first commit

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -6,10 +6,10 @@ export default function About(){
     return(
         <div>
             <Flex className="container">
-                <div className="left">
+                <div className="left-us">
                     <img src={newpic} alt="emp1" />
                 </div>
-                <div className="right">
+                <div className="right-about">
                     <h1>About us</h1>
                     <p>
                     Lytis Software Private Limited is an Indian-based software development and IT services company, incorporated on November 10, 2022, with its headquarters in Rajasthan, India. As a privately held company, Lytis specializes in custom software development, web development, mobile app development, and IT consulting, catering to diverse client needs.
